edddf948a49b3212a0ad958c7a5f21b90ef1e9f0
GodMakesMe <76955563+GodMakesMe@users.noreply.github.com>
2024-04-18 09:57:42 +0530

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -102,7 +102,7 @@ def binary_to_decimal(binary, type_of_bit = "signed"):
         return binary_to_decimal(binary, "signed")
 # registers = {"zero":x0, "ra":r2, "sp":r3, "gp":0, "tp":0, "t0":0, "":0, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31}
 def check_inst(line):
-    if (line=="00000000000000000010000000000000"):
+    if (line=="00000000000000000000000001100011"):
         return ["halt","bonus"]
     if (line=="00000000000000000000000000000000"):
         return ["rst","bonus"]
@@ -424,7 +424,7 @@ def input1():
     line=data.split("\n")
     if line[-1] == "":
         line.remove('')
-    # line.append("00000000000000000000000001100011")
+    line.append("00000000000000000000000001100011")
     program_line = 0
     while (program_line <= len(line)):
         program_line = int(program_line)
